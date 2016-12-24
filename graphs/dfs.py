00c7c1177974aa712ff2313f0e2467ef08a0c0a2
Sandeep Shukla <sandeepshukla31@gmail.com>
2016-12-24 00:55:25 -0800

    Re-write DFS code to print correct result

--- graphs/dfs.py
+++ graphs/dfs.py
@@ -1,19 +1,68 @@
-graph = {'A': set(['B', 'C']),
-         'B': set(['A', 'D', 'E']),
-         'C': set(['A', 'F']),
-         'D': set(['B']),
-         'E': set(['B', 'F']),
-         'F': set(['C', 'E'])}
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+graph2 = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'E'],
+    'D': ['B', 'E', 'F'],
+    'E': ['B', 'C', 'D', 'F'],
+    'F': ['D', 'E']
+}
+
+graph3 = {
+    0: [1, 2],
+    1: [2],
+    2: [0, 3],
+    3: [3]
+}
+
+
+def dfs_iterative(input_graph, start):
+    """ Depth first search result """
+    visited = set()
+    stack = [start]
+    result = []
+
+    while stack:
+        node = stack.pop()
+        if node not in visited:
+            visited.add(node)    # faster search
+            result.append(node)  # maintains order
+            for adj_node in reversed(input_graph[node]):
+                stack.append(adj_node)
+
+
+    return result
+
+def dfs_recursive(input_graph, start, visited=None, result=None):
+    if not visited:
+        visited = set([start])
+    if not result:
+        result = [start]
+
+    for adj_node in input_graph[start]:
+        if adj_node not in visited:
+            visited.add(adj_node)
+            result.append(adj_node)
+            dfs_recursive(input_graph, adj_node, visited, result)
+
+
+    return result
 
 
 
 
-def dfs(graph, start, visited=None):
-    if visited is None:
-        visited = set()
-    visited.add(start)
-    for next in graph[start] - visited:
-        dfs(graph, next, visited)
-    return visited
+print dfs_iterative(graph, 'A')
+print dfs_iterative(graph, 'C')
+print dfs_iterative(graph3, 2)
 
-dfs(graph, 'C') # {'E', 'D', 'F', 'A', 'C', 'B'}
+print dfs_recursive(graph, 'A')
+print dfs_recursive(graph, 'C')
+print dfs_recursive(graph3, 2)
